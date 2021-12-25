a4f249e47be412582d8d0e1826071ef44ee3543c
王哈哈 <wanghaha@wanghahadeMacBook-Pro.local>
2021-12-25 09:32:18 +0800

    update

---
+++ christmas.go
@@ -0,0 +1,71 @@
+package christmas
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/go-redis/redis/v8"
+	"github.com/wanghaha-dev/u"
+	"sync"
+)
+
+type Task struct {
+	Id           string `json:"id"`
+	Group        string `json:"group"`
+	Name         string `json:"name"`
+	Status       int    `json:"status"`
+	Data         string `json:"data"`
+	ReadTime     string `json:"read_time"`
+	CompleteTime string `json:"complete_time"`
+}
+
+var _task *Task
+var _taskOnce sync.Once
+
+func New() *Task {
+	_taskOnce.Do(func() {
+		_task = new(Task)
+	})
+	return _task
+}
+
+// AddTask add task
+func AddTask(ctx context.Context, client *redis.Client, group string, task *Task) string {
+	jsonData, _ := json.Marshal(task)
+	client.LPush(ctx, group+"_untreated", jsonData)
+	return task.Id
+}
+
+// QueryTask query task
+func QueryTask(ctx context.Context, client *redis.Client, taskId string) *Task {
+	taskData := client.HGet(ctx, "completed", taskId)
+	var task Task
+	json.Unmarshal([]byte(taskData.Val()), &task)
+	return &task
+}
+
+// AddConsumer add consumer
+func AddConsumer(ctx context.Context, client *redis.Client, group string, handle func(t *Task)) {
+	for {
+		u.NewColor(u.FgLightWhite, u.BgBlue).Println(u.Time().DateTime(), "Waiting to work ...")
+		getTask := client.BRPop(ctx, 0, group+"_untreated")
+		var task Task
+		json.Unmarshal([]byte(getTask.Val()[1]), &task)
+		task.ReadTime = u.Time().DateTime()
+
+		u.Blue.Println("(1/4)", u.Time().DateTime(), "read task ", task.Id, "...")
+
+		// execute
+		u.Magenta.Println("(2/4)", u.Time().DateTime(), "execute task ", task.Id, "...")
+		handle(&task)
+
+		// modify status
+		u.Cyan.Println("(3/4)", u.Time().DateTime(), "update task status", task.Id, "...")
+		task.CompleteTime = u.Time().DateTime()
+		task.Status = 200
+
+		// Completed
+		completedTask, _ := json.Marshal(task)
+		client.HSet(ctx, "completed", task.Id, completedTask)
+		u.Green.Println("(4/4)", u.Time().DateTime(), task.Id, "completed ...")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,43 @@
+module github.com/wanghaha-dev/christmas
+
+go 1.17
+
+require (
+	github.com/go-redis/redis/v8 v8.11.4
+	github.com/wanghaha-dev/u v0.0.0-20211224172455-47482a160c64
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-basic/uuid v1.0.0 // indirect
+	github.com/golang-module/carbon/v2 v2.0.1 // indirect
+	github.com/gookit/color v1.5.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.10.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/tidwall/gjson v1.12.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
